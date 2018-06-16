1d8a7cdb239c4dc912fb00087abe2e7127295f47
vivian9735759 <56568481@qq.com>
2018-06-17 06:32:23 +0800

    BuildNumber=20180616.101

--- srcs/TFS_Codes/ConsoleApp/ConsoleApp/Program.cs
+++ srcs/TFS_Codes/ConsoleApp/ConsoleApp/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("f'f'd's'g'f'd'g");
+            Console.WriteLine("a");
         }
     }
 }
